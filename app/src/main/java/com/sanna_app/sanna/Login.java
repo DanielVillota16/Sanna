c3c0ec9b792d15d43fbd7991acb0a8b2179e3d77
Millionhat <38446282+Millionhat@users.noreply.github.com>
2020-12-12 16:52:59 -0500

    se agrego el login son google(No se si las credenciales estan funcionando)

--- app/src/main/java/com/sanna_app/sanna/Login.java
+++ app/src/main/java/com/sanna_app/sanna/Login.java
@@ -1,5 +1,6 @@
 package com.sanna_app.sanna;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,17 +28,29 @@ import com.sanna_app.sanna.model.User;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
+    public static int LOGIN=-1;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
     private EditText emailET;
     private EditText passwordET;
     private Button loginBtn;
     private Button signUpBtn;
     private FirebaseFirestore db;
+    private SignInButton google;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        google = findViewById(R.id.GLogIn);
+        google.setSize(SignInButton.SIZE_STANDARD);
+
         mAuth = FirebaseAuth.getInstance();
         emailET = findViewById(R.id.emailET);
         passwordET = findViewById(R.id.passwordET);
@@ -41,6 +60,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         loginBtn.setOnClickListener(this);
         signUpBtn.setOnClickListener(this);
+        google.setOnClickListener(this);
     }
 
     public void goToHomeScreen(FirebaseUser user) {
@@ -51,25 +71,28 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                     /**for(DocumentSnapshot doc : task.getResult()){
                         u = doc.toObject(User.class);
                     }*/
-
-                    if(u.getRole() != 0) {
-                        Intent i;
-                        switch (u.getRole()){
-                            case Constants.CLIENT_ROLE:
-                                i = new Intent(this, ClientHome.class);
-                                break;
-                            case Constants.PROVIDER_ROLE:
-                                i = new Intent(this, ProviderHome.class);
-                                break;
-                            default:
-                                i = new Intent(this, DeliveryHome.class);
-                                break;
-                        }
-                        startActivity(i);
-                        this.finish();
-                    } else
-                        Toast.makeText(this, "No se encontró el usuario!", Toast.LENGTH_LONG).show();
+                    sortRole(u.getRole());
                 });
+    }
+
+    private void sortRole(int u) {
+        if(u != 0) {
+            Intent i;
+            switch (u){
+                case Constants.CLIENT_ROLE:
+                    i = new Intent(this, ClientHome.class);
+                    break;
+                case Constants.PROVIDER_ROLE:
+                    i = new Intent(this, ProviderHome.class);
+                    break;
+                default:
+                    i = new Intent(this, DeliveryHome.class);
+                    break;
+            }
+            startActivity(i);
+            this.finish();
+        } else
+            Toast.makeText(this, "No se encontró el usuario!", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -101,6 +124,58 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 Intent intent = new Intent(this, SignUpActivity.class);
                 startActivity(intent);
                 break;
+
+            case R.id.GLogIn:
+                gLogIn();
+                break;
+        }
+    }
+
+    private void gLogIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, LOGIN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==LOGIN){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try{
+            GoogleSignInAccount account= task.getResult();
+            db.collection("users").document(account.getId()).get().addOnCompleteListener(
+                    t->{
+                        if(t.isSuccessful()){
+                            DocumentSnapshot doc=t.getResult();
+                            User u=doc.toObject(User.class);
+                            sortRole(u.getRole());
+                        }else{
+                            User newU=new User();
+                            newU.setId(account.getId());
+                            newU.setName(account.getDisplayName());
+                            newU.setEmail(account.getEmail());
+                            newU.setAddress("");
+                            newU.setRole(Constants.CLIENT_ROLE);
+                            db.collection("users").document(newU.getId()).set(newU).addOnCompleteListener(
+                                    task2->{
+                                        if(task2.isSuccessful()){
+                                            sortRole(newU.getRole());
+                                        }
+                                    }
+                            );
+                        }
+
+
+                    }
+            );
+        }catch (Exception e){
+            Toast.makeText(this,""+e.getMessage(),Toast.LENGTH_LONG);
         }
     }
 }
