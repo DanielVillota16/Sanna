fa6e3f651f1d8f986c9f0f1043dc3f02ddc6c2ef
Juan David Payán <JuanDavidPF@gmail.com>
2020-12-13 11:58:59 -0500

    Created Bottom navigation bar

--- app/src/main/java/com/sanna_app/sanna/ClientHome.java
+++ app/src/main/java/com/sanna_app/sanna/ClientHome.java
@@ -1,10 +1,19 @@
 package com.sanna_app.sanna;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ClientHome extends AppCompatActivity {
+
+
+    ConstraintLayout frameContainer;
+    BottomNavigationView navBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,11 @@ public class ClientHome extends AppCompatActivity {
         setContentView(R.layout.activity_client_home);
 
 
-    }
-}
+        frameContainer = findViewById(R.id.clientHome_frameContainer);
+        navBar = findViewById(R.id.clientHome_bottomNavigationView);
+
+
+    }//closes onCreate method
+
+
+}//closes ClientHome class
