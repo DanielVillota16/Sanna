3badce43a739d58c2478e5d6bdb7a1ec9ee98012
jhonrs <jhonrs812@gmail.com>
2020-12-13 19:01:32 -0500

    creación, actividad repartidor, vistas, perfil, pedidos y configuración de cuenta

---
+++ app/src/main/java/com/sanna_app/sanna/ProfileFragment.java
@@ -0,0 +1,69 @@
+package com.sanna_app.sanna;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends Fragment {
+
+private Button btnlist;
+private Button btnConfig;
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+       fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnlist =  root.findViewById(R.id.btnpedidos);
+        btnConfig = root.findViewById(R.id.btnconfig);
+
+
+        btnlist.setOnClickListener(
+                (v)->{
+                    Intent i = new Intent(getActivity(), ListaPedido.class);
+                    startActivity(i);
+                }
+        );
+
+        btnConfig.setOnClickListener(
+                (v)->{
+                    Intent i = new Intent(getActivity(), ConfigProfile.class);
+                    startActivity(i);
+                }
+        );
+
+        // Inflate the layout for this fragment
+        return root;
+    }
+
+
+
+
+}
